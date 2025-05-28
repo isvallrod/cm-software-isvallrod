480ec7d122b4d95262ec41869975f304bde7d0a6
isvallrod <kape1296@gmail.com>
2025-05-28 11:57:53 -0600

    Correccion en clase Item.java 1. Modificacion de los atributos de la clase Item a privado para incluir encapsulamient 2. Creacion de getters/setters para los atributos para mejorar la seguridad y el control de estado 3. Cracion de metodos de comportamiento Creacion de documento README.md 1. Se crea el documento y se incluye la informacion de las malas practicas y las correcciones

--- CodigoLegadoJava/Item.java
+++ CodigoLegadoJava/Item.java
@@ -2,17 +2,37 @@ package com.gildedrose;
 
 public class Item {
 
-    public String name;
-
-    public int sellIn;
-
-    public int quality;
+    private String name;
+    private int sellIn;
+    private int quality;
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    // Ejemplo de metodos de comportamiento
+    public void increaseQuality() {
+        if (quality < 50) quality++;
+    }
+    
+    public void decreaseSellIn() {
+        sellIn--;
+    }
+
 
    @Override
    public String toString() {
---
+++ CodigoLegadoJava/README.md
@@ -0,0 +1,14 @@
+# Tarea 2
+# Identificacion y Correccion de Malas Practicas
+
+Para esta tarea se van a utilizar las clases de Java del siguiente repositorio como referencia:
+https://github.com/emilybache/GildedRose-Refactoring-Kata/tree/main/Java
+
+## Identificacion de Malas practicas en las clases:
+### Clase GildedRose.java
+### Clase Item.java
+### 1. Atributos publicos
+Se accede y modifica directamente name, sellIn y quality
+### 2. Clase sin metodos de comportamiento
+La clase unicamente mantiene el estado de los atributos, pero
+no contiene nada relaciondo a la logica
