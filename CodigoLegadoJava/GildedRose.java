6acb8334d0c40757909a897a84b2ccc387339d78
isvallrod <kape1296@gmail.com>
2025-05-28 12:53:50 -0600

    Correcion de la clase GildedRose 1. Se agrupan las reglas por tipo de item para mejor legibilidad 2. Se elimina el uso directo de los atributos de item 3. Se encapsula la logica comun en diferentes funciones

--- CodigoLegadoJava/GildedRose.java
+++ CodigoLegadoJava/GildedRose.java
@@ -12,64 +12,41 @@ class GildedRose {
                
             switch (item.getName()) {
                 case "Aged Brie":
+                    updateAgedBrie(item);
                     break;
                 case "Backstage passes to a TAFKAL80ETC concert":
+                    updateBackstagePass(item);
                     break;
                 case "Sulfuras, Hand of Ragnaros":
                     break;
                 default:
-                break;
+                    updateNormalItem(item);
             }
-
-
-            //if (!items[i].name.equals("Aged Brie")
-            //        && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            //    if (items[i].quality > 0) {
-            //        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-            //            items[i].quality = items[i].quality - 1;
-            //        }
-            //    }
-            //} else {
-            //    if (items[i].quality < 50) {
-            //        items[i].quality = items[i].quality + 1;
-//
-            //        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            //            if (items[i].sellIn < 11) {
-            //                if (items[i].quality < 50) {
-            //                    items[i].quality = items[i].quality + 1;
-            //                }
-            //            }
-//
-            //            if (items[i].sellIn < 6) {
-            //                if (items[i].quality < 50) {
-            //                    items[i].quality = items[i].quality + 1;
-            //                }
-            //            }
-            //        }
-            //    }
-            //}
-//
-            //if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-            //    items[i].sellIn = items[i].sellIn - 1;
-            //}
-//
-            //if (items[i].sellIn < 0) {
-            //    if (!items[i].name.equals("Aged Brie")) {
-            //        if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            //            if (items[i].quality > 0) {
-            //                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-            //                    items[i].quality = items[i].quality - 1;
-            //                }
-            //            }
-            //        } else {
-            //            items[i].quality = items[i].quality - items[i].quality;
-            //        }
-            //    } else {
-            //        if (items[i].quality < 50) {
-            //            items[i].quality = items[i].quality + 1;
-            //        }
-            //    }
-            //}
         }
     }
-}
+
+    private void updateNormalItem(Item item) {
+        item.decreaseQuality();
+        item.decreaseSellIn();
+        if (item.isExpired()) {
+            item.decreaseQuality();
+        }
+    }
+
+    private void updateAgedBrie(Item item) {
+        item.increaseQuality();
+        item.decreaseSellIn();
+        if (item.isExpired()) {
+            item.increaseQuality();
+        }
+    }
+
+    private void updateBackstagePass(Item item) {
+        item.increaseQuality();
+        if (item.getSellIn() <= 10) item.increaseQuality();
+        if (item.getSellIn() <= 5) item.increaseQuality();
+
+        item.decreaseSellIn();
+    }
+
+ }
