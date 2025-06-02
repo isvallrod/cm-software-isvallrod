c056552c344133a649f987eeff67e6447084c3f2
isvallrod <kape1296@gmail.com>
2025-06-01 23:35:54 -0600

    Actualizacion final de la clase GildedRose
    GildedRose ahora solo tiene una responsabilidad que es iterar
    sobre los diferentes tipos de item existentes
    Se eliminan las clases por tipo que se sustituyeron por clases
    especificas segun el tipo de item

--- CodigoLegadoJava/GildedRose.java
+++ CodigoLegadoJava/GildedRose.java
@@ -1,52 +1,17 @@
 package com.gildedrose;
 
 class GildedRose {
-    Item[] items;
+    private Item[] items;
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-               
-            switch (item.getName()) {
-                case "Aged Brie":
-                    updateAgedBrie(item);
-                    break;
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    updateBackstagePass(item);
-                    break;
-                case "Sulfuras, Hand of Ragnaros":
-                    break;
-                default:
-                    updateNormalItem(item);
-            }
-        }
+    for (Item item : items) {
+        ItemUpdater updater = ItemUpdaterFactory.create(item);
+        updater.update();
     }
-
-    private void updateNormalItem(Item item) {
-        item.decreaseQuality();
-        item.decreaseSellIn();
-        if (item.isExpired()) {
-            item.decreaseQuality();
-        }
-    }
-
-    private void updateAgedBrie(Item item) {
-        item.increaseQuality();
-        item.decreaseSellIn();
-        if (item.isExpired()) {
-            item.increaseQuality();
-        }
-    }
-
-    private void updateBackstagePass(Item item) {
-        item.increaseQuality();
-        if (item.getSellIn() <= 10) item.increaseQuality();
-        if (item.getSellIn() <= 5) item.increaseQuality();
-
-        item.decreaseSellIn();
     }
 
  }
